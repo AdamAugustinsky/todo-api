1474d938b88aa00a3830e1021955505b553bf90e
Adam Axel Augustinsky <40921659+AdamAugustinsky@users.noreply.github.com>
2021-03-10 17:47:04 -0300

    feat(createUser)

--- src/index.js
+++ src/index.js
@@ -1,21 +1,32 @@
 const express = require('express');
 const cors = require('cors');
-
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { user, username } = request.body; 
+
+  const id = uuidv4();
+
+  users.push({
+    id,
+    user,
+    username,
+    todos: []
+  });
+
+  console.log(users);
+  return response.status(201).send();
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
